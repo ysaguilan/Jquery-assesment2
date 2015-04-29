b5e287220cab7d0a3040f55d6c9e79dafc4a1143
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-29 08:47:02 -0700

    Jquery Assesment2

---
+++ assesment.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="dan">
+		<button type="button">Click me</button>
+	</div>
+
+	<div class="iwao">
+		<button type="button">Click me</button>
+	</div>
+<script src="main.js"></script>
+<script>
+	$('dan').click(function() {
+		alert('Hey you clicked on me');
+	})
+</script>
+</body>
+</html>
